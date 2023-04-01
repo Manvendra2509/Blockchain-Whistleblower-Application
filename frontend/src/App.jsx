8410452973e18859767f4b86962fbb02e1591234
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-01 19:34:55 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+       <h1>Hello</h1>
+       <p>You can start the project by editing src/App.jsx</p>
+    </div>
+  )
+}
+
+export default App
