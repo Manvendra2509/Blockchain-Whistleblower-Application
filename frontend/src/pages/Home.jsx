e9698afa2bada0c2614a0b21701177e24bc78378
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-05 21:36:52 +0530

    Login component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Login from './Auth';
+
 const Home = () => {
   return (
     <div className="navbar bg-neutral text-neutral-content">
       <a className="btn btn-ghost normal-case text-xl">WhistleBlower</a>
+      <Login/>
     </div>
   );
 };
