afc5f7c4d3f0a367c5d23aeeecf3bd90eaea1076
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-07 22:43:48 +0530

    Form page route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages/Home";
+import Form from "./pages/Form";
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/form" element={<Form/>}/>
       </Routes>
     </div>
   );
